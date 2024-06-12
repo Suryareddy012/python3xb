dfe5699c994833239fe5e19cc5320db394a33629
Suryareddy <prakashreddy1299@gmail.com>
2024-06-12 22:03:06 +0530

    Revert "Delete Day004_10062024_TASK directory"
    This reverts commit a6e176727defe3042ac75853d995a052ab8edfaf.

---
+++ Day004_10062024_TASK/Task01.py
@@ -0,0 +1,7 @@
+#= is Assignment operator, it will assign value to the varibale
+a=10
+# == is comare operator, it will compare value
+
+a = 10
+b = 10
+print(a == b)
---
+++ Day004_10062024_TASK/Task02.py
@@ -0,0 +1,8 @@
+# **
+#it is use to find the power
+a=2
+b=3
+print(a**b)
+print(b**a)
+print(a**a)
+print(b**b)
